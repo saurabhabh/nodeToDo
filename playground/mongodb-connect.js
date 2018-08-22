8674e05e9906006704818c7929b2bb4a056e071d
saurabh <saurabh.abhyankar7@gmail.com>
2018-08-22 17:14:11 +0530

    Added the first Post call

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,30 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db)=>{
-    if(err){
-        console.log("Unable to connect to mongo db server.")
-        return;
-    }
-    console.log("Connect to mongodb server.")
-    // db.collection("Todos").insertOne({
-    //     text: "Something to do",
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err){
-    //        return console.log("Unable to insert.", err)
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // })
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    db.collection("Users").insertOne({
-        name: "Saurabh Abhyankar",
-        age: 25,
-        location: "India"
-    }, (err, results)=>{
-        if(err){
-            return console.log("Unable to insert Users.", err)
-        }
-        console.log(JSON.stringify(results.ops[0]._id.getTimestamp(), undefined, 2));
-    })
-    db.close();
-})
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+});
